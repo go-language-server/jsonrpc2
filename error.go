86cfe05c7009bbd44dedf9a652daefcc60c6f7e2
Koichi Shiraishi <zchee.io@gmail.com>
2019-03-09 15:05:13 +0900

    error: rename ErrorCode to Code

--- error.go
+++ error.go
@@ -8,29 +8,29 @@ import (
 	"fmt"
 )
 
-type ErrorCode int64
+type Code int64
 
 const (
-	CodeParseError           ErrorCode = -32700
-	CodeInvalidRequest       ErrorCode = -32600
-	CodeMethodNotFound       ErrorCode = -32601
-	CodeInvalidParams        ErrorCode = -32602
-	CodeInternalError        ErrorCode = -32603
-	CodeServerErrorStart     ErrorCode = -32099
-	CodeServerErrorEnd       ErrorCode = -32000
-	CodeServerNotInitialized ErrorCode = -32002
-	CodeUnknownErrorCode     ErrorCode = -32001
+	CodeParseError           Code = -32700
+	CodeInvalidRequest       Code = -32600
+	CodeMethodNotFound       Code = -32601
+	CodeInvalidParams        Code = -32602
+	CodeInternalError        Code = -32603
+	CodeServerErrorStart     Code = -32099
+	CodeServerErrorEnd       Code = -32000
+	CodeServerNotInitialized Code = -32002
+	CodeUnknownErrorCode     Code = -32001
 
 	// Defined by the protocol.
-	CodeRequestCancelled ErrorCode = -32800
-	CodeContentModified  ErrorCode = -32801
+	CodeRequestCancelled Code = -32800
+	CodeContentModified  Code = -32801
 )
 
 // Error
 type Error struct {
 
 	// Code a number indicating the error type that occurred.
-	Code ErrorCode `json:"code"`
+	Code Code `json:"code"`
 
 	// Data a Primitive or Structured value that contains additional
 	// information about the error. Can be omitted.
@@ -49,7 +49,7 @@ func (e *Error) Error() string {
 
 // Errorf builds a Error struct for the suppied message and code.
 // If args is not empty, message and args will be passed to Sprintf.
-func Errorf(code ErrorCode, format string, args ...interface{}) *Error {
+func Errorf(code Code, format string, args ...interface{}) *Error {
 	return &Error{
 		Code:    code,
 		Message: fmt.Sprintf(format, args...),
