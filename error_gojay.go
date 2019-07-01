6deeb9bb0c2177193b1a48c2101f9b405f18b0cf
Koichi Shiraishi <zchee.io@gmail.com>
2019-07-01 20:14:29 +0900

    error: rename gojay method reciever name and compile time check pragma

--- error_gojay.go
+++ error_gojay.go
@@ -9,24 +9,30 @@ package jsonrpc2
 import "github.com/francoispqt/gojay"
 
 // UnmarshalJSONObject implements gojay's UnmarshalerJSONObject
-func (v *Error) UnmarshalJSONObject(dec *gojay.Decoder, k string) error {
+func (e *Error) UnmarshalJSONObject(dec *gojay.Decoder, k string) error {
 	switch k {
 	case keyCode:
-		return dec.Int64((*int64)(&v.Code))
+		return dec.Int64((*int64)(&e.Code))
 	case keyMessage:
-		return dec.String(&v.Message)
+		return dec.String(&e.Message)
 	}
 	return nil
 }
 
 // NKeys returns the number of keys to unmarshal
-func (v *Error) NKeys() int { return 2 }
+func (e *Error) NKeys() int { return 2 }
 
 // MarshalJSONObject implements gojay's MarshalerJSONObject
-func (v *Error) MarshalJSONObject(enc *gojay.Encoder) {
-	enc.IntKey(keyCode, int(v.Code))
-	enc.StringKey(keyMessage, v.Message)
+func (e *Error) MarshalJSONObject(enc *gojay.Encoder) {
+	enc.IntKey(keyCode, int(e.Code))
+	enc.StringKey(keyMessage, e.Message)
 }
 
 // IsNil returns wether the structure is nil value or not
-func (v *Error) IsNil() bool { return v == nil }
+func (e *Error) IsNil() bool { return e == nil }
+
+// compile time check whether the Error implements a gojay.MarshalerJSONObject interface.
+var _ gojay.MarshalerJSONObject = (*Error)(nil)
+
+// compile time check whether the Error implements a gojay.UnmarshalerJSONObject interface.
+var _ gojay.UnmarshalerJSONObject = (*Error)(nil)
