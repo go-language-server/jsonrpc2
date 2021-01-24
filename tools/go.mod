dd62c4ca0b3a64ea60045c6c78e8196ae4fd16fb
Koichi Shiraishi <zchee.io@gmail.com>
2021-01-25 04:04:28 +0900

    tools: add mvdan.cc/gofumpt@v0.1.0

--- tools/go.mod
+++ tools/go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/golangci/golangci-lint v1.35.2
 	gotest.tools/gotestsum v1.6.1
+	mvdan.cc/gofumpt v0.1.0
 )
--- tools/tools.go
+++ tools/tools.go
@@ -5,4 +5,6 @@ package tools
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "gotest.tools/gotestsum"
+	_ "mvdan.cc/gofumpt"
+	_ "mvdan.cc/gofumpt/gofumports"
 )
