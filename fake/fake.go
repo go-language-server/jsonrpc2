b5248d2000c194ca57866cbe3d3aae05fc1dc0c8
Koichi Shiraishi <zchee.io@gmail.com>
2021-07-05 10:27:41 +0900

    all: fix SPDX-FileCopyrightText lisence header
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- fake/fake.go
+++ fake/fake.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright 2021 The Go Language Server Authors
+// SPDX-FileCopyrightText: 2021 The Go Language Server Authors
 // SPDX-License-Identifier: BSD-3-Clause
 
 // Package fake provides utilities for running tests against a remote LSP server.
